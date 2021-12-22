aee8e3602da2ed0ec6ef7761fe442ac9ddade23f
Adham Khatean <aekhatean@gmail.com>
2021-12-23 00:06:08 +0200

    Create processImage, createThumbnail, thumbnailExists, imageExists functions for the middleware

--- src/middlewares/processImage.mw.ts
+++ src/middlewares/processImage.mw.ts
@@ -1,28 +1,71 @@
 import express from 'express';
+import fs from 'fs';
+import sharp from 'sharp';
 
-type imageObject = {
-  imageName: string;
-  height: string;
-  width: string;
+/**
+ * Function checks if image exists in images directory
+ * @param name original image name
+ * @returns boolean
+ */
+const imageExists = (name: string): boolean => {
+  const imagePath = `../../data/images/${name}.jpg`;
+  if (fs.existsSync(imagePath)) {
+    return true;
+  }
+  return false;
 };
 
-const getQueryParams = (req: express.Request): imageObject => {
-  return {
-    imageName: req.query.imageName as unknown as string,
-    height: req.query.height as unknown as string,
-    width: req.query.width as unknown as string
-  };
+/**
+ * Function checks if a thumbnail was created before
+ * @param name thumbnail image name
+ * @returns boolean
+ */
+const thumbnailExists = (name: string): boolean => {
+  const thumbnailPath = `../../data/thumbnails/${name}.jpg`;
+  if (fs.existsSync(thumbnailPath)) {
+    return true;
+  }
+  return false;
+};
+const createThumbnail = (
+  image: string,
+  width: string,
+  height: string
+): void => {
+  const imagePath = `../../data/images/${image}.jpg`;
+  const thumbnailPath = `../../data/thumbnails/${image}-${width}-${height}.jpg`;
+  const iWidth = parseInt(width);
+  const iHeight = parseInt(height);
+
+  sharp(imagePath)
+    .resize(iWidth, iHeight, { fit: 'contain' })
+    .toFile(thumbnailPath);
 };
 
+/**
+ * Function creates a resized version (thumbnail) of an image
+ * @param req
+ * @param res
+ * @param next
+ */
 const processImage = (
   req: express.Request,
   res: express.Response,
   next: express.NextFunction
 ): void => {
-  const { imageName, height, width } = getQueryParams(req);
+  const { imageName, height, width } = req.query;
+  if (imageExists(imageName as unknown as string)) {
+    console.log('hellodd there');
+    const thumbnailName = `${imageName}-${height}-${width}`;
+    if (!thumbnailExists(thumbnailName)) {
+      createThumbnail(
+        imageName as unknown as string,
+        width as unknown as string,
+        height as unknown as string
+      );
+    }
+  }
   next();
 };
 
-// checkExistantThumbname.util
-// createThumbnail.util
-export { processImage, getQueryParams };
+export default processImage;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,15 @@
 import express from 'express';
+import processImage from '../middlewares/processImage.mw';
 
 const routes = express.Router();
+routes.use(processImage);
 
 routes.get('/', (req, res) => {
   res.redirect('./api');
 });
 
-routes.get('/api', (req, res) => {
-  res.send(req.query.filename);
+routes.get('/api', processImage, (req, res) => {
+  res.send(req.query);
 });
 
 export default routes;
